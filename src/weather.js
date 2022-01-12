0d683feea64c269a5c602debf90f5a707531f52d
David Turchetta <davidturchetta@outlook.com>
2022-01-12 15:49:54 -0500

    Attempted fix of api call

--- src/weather.js
+++ src/weather.js
@@ -31,7 +31,7 @@ async function getCoordinates(city){
   const lat = weatherData.city.coord.lat;
   const lon = weatherData.city.coord.lon;
   const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=0ad552a59c7b844bcd402e711e49d1d6`;
-  getWeather(url);
+  return getWeather(url);
 }
 
 async function getWeather(url){
